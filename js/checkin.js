37d1ea28402338b30a373a8bfaddd79effbfde29
muhammedenesakdag76-ui <muhammedenesakdag76@gmail.com>
2026-07-26 13:23:38 +0300

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -1,1 +1,125 @@
-f
+import {
+    kayitNoIleBul,
+    checkinYap
+} from "./firebase.js";
+
+let aktifKayit = null;
+
+const resultCard = document.getElementById("resultCard");
+
+const rKayitNo = document.getElementById("rKayitNo");
+const rAd = document.getElementById("rAd");
+const rTelefon = document.getElementById("rTelefon");
+const rDurum = document.getElementById("rDurum");
+
+const manualSearch = document.getElementById("manualSearch");
+const manualCode = document.getElementById("manualCode");
+const checkinButton = document.getElementById("checkinButton");
+
+function goster(kayit){
+
+    aktifKayit = kayit;
+
+    resultCard.classList.remove("hidden");
+
+    rKayitNo.textContent = kayit.kayitNo;
+    rAd.textContent = kayit.adSoyad;
+    rTelefon.textContent = kayit.telefon;
+
+    if(kayit.checkin){
+
+        rDurum.textContent = "✅ Giriş Yapılmış";
+
+        checkinButton.disabled = true;
+        checkinButton.textContent = "Check-in Tamamlandı";
+
+    }else{
+
+        rDurum.textContent = "❌ Bekliyor";
+
+        checkinButton.disabled = false;
+        checkinButton.textContent = "Check-in Yap";
+
+    }
+
+}
+
+async function ara(kayitNo){
+
+    const kayit = await kayitNoIleBul(kayitNo);
+
+    if(!kayit){
+
+        alert("Katılımcı bulunamadı.");
+
+        return;
+
+    }
+
+    goster(kayit);
+
+}
+
+manualSearch.addEventListener("click",()=>{
+
+    const kod = manualCode.value.trim();
+
+    if(kod===""){
+
+        alert("Kayıt numarası giriniz.");
+
+        return;
+
+    }
+
+    ara(kod);
+
+});
+
+checkinButton.addEventListener("click",async()=>{
+
+    if(!aktifKayit) return;
+
+    await checkinYap(aktifKayit.id);
+
+    alert("Check-in başarıyla tamamlandı.");
+
+    aktifKayit.checkin = true;
+
+    goster(aktifKayit);
+
+});
+
+const scanner = new Html5Qrcode("reader");
+
+scanner.start(
+
+{ facingMode:"environment" },
+
+{
+
+fps:10,
+
+qrbox:250
+
+},
+
+async(decodedText)=>{
+
+try{
+
+const veri = JSON.parse(decodedText);
+
+await ara(veri.kayitNo);
+
+scanner.stop();
+
+}catch{
+
+alert("Geçersiz QR kodu.");
+
+}
+
+}
+
+);
