2504b05cb50c495e1aac97c6164fa1d502e1217c
muhammedenesakdag76-ui <muhammedenesakdag76@gmail.com>
2026-07-26 13:06:35 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,197 @@
-g
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+
+getFirestore,
+
+collection,
+
+addDoc,
+
+getDocs,
+
+getDoc,
+
+doc,
+
+deleteDoc,
+
+updateDoc,
+
+query,
+
+where,
+
+serverTimestamp
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig={
+
+apiKey:"",
+
+authDomain:"",
+
+projectId:"",
+
+storageBucket:"",
+
+messagingSenderId:"",
+
+appId:""
+
+};
+
+const app=initializeApp(firebaseConfig);
+
+const db=getFirestore(app);
+
+const COLLECTION="kayitlar";
+
+export{db};
+
+export async function tcVarMi(tc){
+
+const q=query(
+
+collection(db,COLLECTION),
+
+where("tc","==",tc)
+
+);
+
+const sonuc=await getDocs(q);
+
+return !sonuc.empty;
+
+}
+
+export async function toplamKayit(){
+
+const sonuc=await getDocs(
+
+collection(db,COLLECTION)
+
+);
+
+return sonuc.size;
+
+}
+
+export async function tumKayitlar(){
+
+const sonuc=await getDocs(
+
+collection(db,COLLECTION)
+
+);
+
+const liste=[];
+
+sonuc.forEach(docItem=>{
+
+liste.push({
+
+id:docItem.id,
+
+...docItem.data()
+
+});
+
+});
+
+return liste;
+
+}
+
+export async function kayitOlustur(veri){
+
+return await addDoc(
+
+collection(db,COLLECTION),
+
+{
+
+...veri,
+
+checkin:false,
+
+checkinSaati:null,
+
+olusturmaTarihi:serverTimestamp()
+
+}
+
+);
+
+}
+
+export async function kayitSil(id){
+
+await deleteDoc(
+
+doc(db,COLLECTION,id)
+
+);
+
+}
+
+export async function checkinYap(id){
+
+await updateDoc(
+
+doc(db,COLLECTION,id),
+
+{
+
+checkin:true,
+
+checkinSaati:serverTimestamp()
+
+}
+
+);
+
+}
+
+export async function checkinIptal(id){
+
+await updateDoc(
+
+doc(db,COLLECTION,id),
+
+{
+
+checkin:false,
+
+checkinSaati:null
+
+}
+
+);
+
+}
+
+export async function kayitGetir(id){
+
+const belge=await getDoc(
+
+doc(db,COLLECTION,id)
+
+);
+
+if(!belge.exists()){
+
+return null;
+
+}
+
+return{
+
+id:belge.id,
+
+...belge.data()
+
+};
+
+}
