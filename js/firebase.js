4ddc8b63edfe796fed7f64377f899bb0cd599dab
tugvaselcuk <muhammedenesakdag76@gmail.com>
2026-07-27 23:51:56 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -17,19 +17,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey: "",
-
-    authDomain: "",
-
-    projectId: "",
-
-    storageBucket: "",
-
-    messagingSenderId: "",
-
-    appId: ""
-
+  apiKey: "AIzaSyA1PwF_MonQVMQ2zXnCJZbQWYkRgHpxxb8",
+  authDomain: "tugva-kayit-sistemi.firebaseapp.com",
+  projectId: "tugva-kayit-sistemi",
+  storageBucket: "tugva-kayit-sistemi.firebasestorage.app",
+  messagingSenderId: "497137562254",
+  appId: "1:497137562254:web:0dae95a054ac7e21424fdf"
 };
 
 const app = initializeApp(firebaseConfig);
