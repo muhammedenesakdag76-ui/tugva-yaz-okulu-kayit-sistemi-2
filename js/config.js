fb2dffe13d318ef4677b81bc73d5b21658ddcd93
tugvaselcuk <muhammedenesakdag76@gmail.com>
2026-07-28 02:06:36 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,20 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1PwF_MonQVMQ2zXnCJZbQWYkRgHpxxb8",
-  authDomain: "tugva-kayit-sistemi.firebaseapp.com",
-  projectId: "tugva-kayit-sistemi",
-  storageBucket: "tugva-kayit-sistemi.firebasestorage.app",
-  messagingSenderId: "497137562254",
-  appId: "1:497137562254:web:0dae95a054ac7e21424fdf"
+    apiKey: "AIzaSyA1PwF_MonQVMQ2zXnCJZbQWYkRgHpxxb8",
+    authDomain: "tugva-kayit-sistemi.firebaseapp.com",
+    projectId: "tugva-kayit-sistemi",
+    storageBucket: "tugva-kayit-sistemi.firebasestorage.app",
+    messagingSenderId: "497137562254",
+    appId: "1:497137562254:web:0dae95a054ac7e21424fdf"
 };
 
 export const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-
 export const auth = getAuth(app);
+
+export const MAX_CAPACITY = 45;
